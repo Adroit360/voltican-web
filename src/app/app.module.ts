352b186d6c9fc12fc48bbf206f9a066a3684b8eb
Moro-Afriyie <ceremoinc99@gmail.com>
2022-09-19 17:03:25 +0000

    feat:update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { CarrersComponent } from './pages/carrers/carrers.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { CertificationsComponent } from './pages/certifications/certifications.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent,
+    ServicesComponent,
+    AboutUsComponent,
+    CarrersComponent,
+    ContactUsComponent,
+    CertificationsComponent
   ],
   imports: [
     BrowserModule,
