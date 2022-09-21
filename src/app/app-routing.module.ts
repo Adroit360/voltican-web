3ea43166b2e80cc256fe7b537030dc859fa91c98
Moro-Afriyie <ceremoinc99@gmail.com>
2022-09-21 01:20:50 +0000

    feat:create the about us pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { CarrersComponent } from './pages/carrers/carrers.component';
+import { ClienteleComponent } from './pages/about-us/clientele/clientele.component';
+import { OurExpertsComponent } from './pages/about-us/our-experts/our-experts.component';
+import { VolticanEdgeComponent } from './pages/about-us/voltican-edge/voltican-edge.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about/voltican-edge',
+    component: VolticanEdgeComponent,
+  },
+  { path: 'about/clientele', component: ClienteleComponent },
+  { path: 'about/our-experts', component: OurExpertsComponent },
+  { path: 'careers', component: CarrersComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
