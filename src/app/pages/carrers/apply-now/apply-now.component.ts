d05d1e0bccd25abf89381615b7349843dcfe14ef
Moro-Afriyie <ceremoinc99@gmail.com>
2023-01-18 21:50:46 +0000

    feat:update

--- src/app/pages/carrers/apply-now/apply-now.component.ts
+++ src/app/pages/carrers/apply-now/apply-now.component.ts
@@ -11,6 +11,8 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ApplyNowComponent implements OnInit {
   job: Job = {} as Job;
+  isLoading = false;
+  id: string = '';
 
   constructor(
     private firestore: AngularFirestore,
@@ -18,19 +20,27 @@ export class ApplyNowComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.id = params['id'];
+    });
     if (!localStorage.getItem('job')) {
-      this.route.params.subscribe((params) => {
-        let id = params['id'];
-        this.getJob(id).subscribe((item: Job) => {
-          this.job = item;
-        });
+      this.getJob(this.id).subscribe((item: Job) => {
+        this.job = item;
+        this.isLoading = false;
       });
     } else {
       this.job = JSON.parse(localStorage.getItem('job') || '{}');
+      if (this.id !== this.job.jobId) {
+        this.getJob(this.id).subscribe((item: Job) => {
+          this.job = item;
+          this.isLoading = false;
+        });
+      }
     }
   }
 
   getJob(itemId: string) {
+    this.isLoading = true;
     return this.firestore
       .collection('jobs')
       .doc(itemId)
